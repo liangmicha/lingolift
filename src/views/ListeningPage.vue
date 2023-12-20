ee152f9f558cb603f400881ef7eaa315fab8011e
Michael Liang <liangmicha@gmail.com>
2023-12-20 09:49:44 -0800

    Add basic template for listening page

--- src/views/ListeningPage.vue
+++ src/views/ListeningPage.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>
-    <h1>Listening Page</h1>
+  <div class="listening-page">
+    <h1>Listening Exercises</h1>
+    <textarea v-model="userInput" placeholder="Paste your notes here..."></textarea>
+    
+    <div class="dropdown-container">
+      <div class="dropdown-group">
+        <label for="learningLanguage">I'm Learning:</label>
+        <select v-model="learningLanguage" id="learningLanguage">
+          <option value="pt">Portuguese</option>
+          <!-- Add other learning language options here -->
+        </select>
+      </div>
+      
+      <div class="dropdown-group">
+        <label for="primaryLanguage">I'm Fluent In:</label>
+        <select v-model="primaryLanguage" id="primaryLanguage">
+          <option value="en">English</option>
+          <!-- Add other primary language options here -->
+        </select>
+      </div>
+    </div>
+
+    <button @click="generateExercises">Generate learning exercises</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'VocabularyPage'
+<script setup>
+import { ref } from 'vue';
+
+const userInput = ref('');
+const learningLanguage = ref('pt'); // Default learning language
+const primaryLanguage = ref('en'); // Default primary language
+
+const generateExercises = () => {
+  // Logic to be implemented
+  console.log('Exercises generation initiated');
 };
 </script>
+
+<style scoped>
+.listening-page {
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+}
+
+textarea {
+  width: 100%;
+  min-height: 150px;
+  margin: 10px 0;
+}
+
+.dropdown-container {
+  display: flex;
+  gap: 10px;
+  margin: 10px 0;
+}
+
+.dropdown-group {
+  flex: 1;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+select {
+  width: 100%;
+  padding: 10px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: white;
+  font-size: 16px;
+}
+
+button {
+  background-color: var(--primary-color);
+  border: none;
+  border-radius: 4px;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: var(--secondary-color);
+}
+</style>
