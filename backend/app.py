b59f2de1dceeecf64f565ed2f50e964c5a8c910f
Michael Liang <liangmicha@gmail.com>
2023-12-20 16:11:38 -0800

    Update flask side of the app and include gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, send_file
+from gtts import gTTS
+import os
 
 app = Flask(__name__)
 
 @app.route('/get-audio/<phrase>', methods=['GET'])
 def get_audio(phrase):
-    # Your logic to fetch or generate the audio for the phrase
-    # For now, returning a placeholder response
-    return jsonify({"message": "Audio for: " + phrase})
+    tts = gTTS(phrase, lang='en')  # Set the language as needed
+    audio_file = f"{phrase}.mp3"
+    tts.save(audio_file)
+    return send_file(audio_file, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
