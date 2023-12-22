c7c70680a0b37380e7a5e04696f45a2d8c716eb0
Michael Liang <liangmicha@gmail.com>
2023-12-21 16:46:01 -0800

    Add endpoint for returning flashcards, responsibiltiy on the backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, jsonify
 from gtts import gTTS
 import os
+import re
 
 app = Flask(__name__)
 
@@ -10,6 +11,21 @@ def get_audio(phrase):
     audio_file = f"{phrase}.mp3"
     tts.save(audio_file)
     return send_file(audio_file, as_attachment=True)
+
+@app.route('/api/process-notes', methods=['POST'])
+def process_notes():
+    data = request.json
+    raw_notes = data.get('notes', '')
+    
+    # Simple parsing logic (customize as needed)
+    # Splitting notes into sentences/phrases using a regular expression
+    phrases = re.split(r'[.!?]+', raw_notes)
+    phrases = [phrase.strip() for phrase in phrases if phrase.strip()]
+
+    # Structure the data for flashcards
+    flashcards = [{'text': phrase} for phrase in phrases]
+
+    return jsonify({'flashcards': flashcards})
 
 if __name__ == '__main__':
     app.run(debug=True)
